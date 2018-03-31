f39e9a17b1812154d3c498b7533b063c4c7535fd
mpenate0 <mpenate0@gmail.com>
2018-03-31 15:26:28 -0400

    Actual commit for earlier.

---
+++ Table.java
@@ -0,0 +1,23 @@
+public class Table {
+  String name;
+  int seatCount;
+  CustomerParty party;
+
+  public Table(String name, int seatCount) {
+    this.name = name;
+    this.seatCount = seatCount;
+    party = null;
+  }
+
+  public String getName() { return name; }
+
+  public int getSeatCount { return seatCount; }
+  public bool canSeat(int requestedSeats) { return (requestedSeats <= seatCount); }
+
+  public void addParty(CustomerParty party) { this.party = party; }
+  public CustomerParty removeParty() {
+    CustomerParty temp = party;
+    party = null;
+    return party;
+  }
+}
