594533ff0484f8ee2f710a111521818ceca81ada
dixonm74 <37880086+dixonm74@users.noreply.github.com>
2018-04-08 20:33:41 -0400

    Update Restaurant.java

--- Restaurant.java
+++ Restaurant.java
@@ -45,13 +45,74 @@ public class Restaurant {
   }
 
   public void finishServing(String name) {
-
+  booolean found = false;
+  int size = tables.size();
+    //can check if this is empty in the driver (keep track of how many parties have been added  and if == 0 
+    for(int i = 0, i < size; i++)
+      {
+        if(tables.get(i).peekParty().getName().equals(name))
+        {         
+         found = true;
+         System.out.print("Table "+tables.get(i).getName()+" has been freed");
+         System.out.print(tables.get(i).peekParty().toString()+" is leaving the restaurant");
+         tables.get(i).removeParty();
+        }
+      }
+    if(!found)
+      {
+       System.out.println("Party "+name+" is not being served");
+      }
   }
 
   public void addTable(Table table, boolean petOK) {
-
+  int size = tables.size();
+  boolean insertHere = true;
+  if(petOK == true)
+  {
+    if(tables.isEmpty()) {
+          tables.add(0,table);
+          npStart++;  }
+    
+    else if(table.getSeatcount() > tables.get(npStart-1).getSeatcount() {
+           tables.add(npStart, table);
+           npStart++;  }
+    else{
+         for(int i = 0; i < npStart;i++)
+        {
+           if(table.getSeatCount() > tables.get(i).getSeatCount()){
+                insertHere = false;
+                }
+           if(insertHere){
+                tables.add(i, table)
+                npStart++;
+                break;  }                       
+         }     
   }
-
+    System.out.println("Table "+table.getName()+" has been added to the Pet Friendly Section");
+  }
+  else 
+  {
+    if(tables.isEmpty()) {
+          tables.add(0,table);
+           }
+    
+    else if(table.getSeatcount() > tables.get(size-1).getSeatcount() {
+           tables.add(size, table);
+            }
+    else{
+         for(int i = npStart; i < size; i++)
+        {
+           if(table.getSeatCount() > tables.get(i).getSeatCount()){
+                insertHere = false;
+                }
+           if(insertHere){
+                tables.add(i, table)
+                break;  }                       
+         }     
+  }
+    System.out.println("Table "+table.getName()+" has been added to the No Pets Section");
+  }
+}
   public void rmTable(String name, boolean petOK) {
     int start = (petOK)?0:npStart;
     int end = (petOK)?npStart-1:tables.size();
