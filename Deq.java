9f93892eb1f13148526f45b42d98cdc3afc7fb36
dixonm74 <37880086+dixonm74@users.noreply.github.com>
2018-04-18 14:39:50 -0400

    Update Deq.java

--- Deq.java
+++ Deq.java
@@ -3,7 +3,10 @@ public class Deq<T> extends Queue<T> implements ExtendedQueueInterface<T> {
     public Deq() {
         super(); //use super constructor
     }
-
+/**
+*Method for adding an item to the start of the Deq
+*@param item to be added to the start of the Deq
+*/
     public void enqueueFirst(T newItem) throws ExtendedQueueException {
         if (numItems == items.length) //need to resize?
             resize(); //do it!
@@ -12,7 +15,10 @@ public class Deq<T> extends Queue<T> implements ExtendedQueueInterface<T> {
         items[front] = newItem; //put new item in front
         numItems++; //keep track of count
     }
-
+/**
+*Method to remove an item from the end of the DEQ
+*@return returns the item that is removed from the DEQ
+*/
     public T dequeueLast() throws ExtendedQueueException {
         if (numItems == 0) //no items to remove, throw an error
             throw new ExtendedQueueException("ExtendedQueueException on dequeueLast. Stack is empty.");
@@ -25,7 +31,10 @@ public class Deq<T> extends Queue<T> implements ExtendedQueueInterface<T> {
 
         return out; //returnung previously stored reference
     }
-
+/**
+*Method to allow the user to see the item that is in the back of the DEQ
+*@return returns the item that is in the back of the DEQ
+*/
     public T peekLast() throws ExtendedQueueException {
         if (numItems == 0) //nothing to peek? throw an error
             throw new ExtendedQueueException("ExtendedQueueException on peekLast. Stack is empty.");
